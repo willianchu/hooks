a7e3758f5a1fb57239d05dcaa70e518286fa4b00
willianchu <willianchu@hotmail.com>
2022-07-06 14:24:29 -0700

    add two setState

--- 01_useState.js
+++ 01_useState.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function countInitial() {
+  console.log('runs every time the component is rendered');	
+  return 4;
+}
 
 function App() {
+  const [count, setCount] = useState( () => { // anonymous function runs once on component mount
+    console.log('run once');
+    return 4;
+  });
+  const [color, setColor] = useState('red');
+  
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1);
+    setColor('blue');
+  }
+  
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+  }
+
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{color}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
